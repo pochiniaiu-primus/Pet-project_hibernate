378c22252ded3e2ed87d69773907b169e43eba8d
serhiihonchar <55111661+serhiihonchar@users.noreply.github.com>
2021-09-07 15:01:36 +0300

    add SQL Scripts for the project

---
+++ 3.sql
@@ -0,0 +1,16 @@
+CREATE TABLE hibernate_db.details (
+  id int NOT NULL AUTO_INCREMENT,
+  city varchar(15),
+  phone_number varchar(25),
+  email varchar(30), PRIMARY KEY (id)
+);
+
+CREATE TABLE hibernate_db.employees (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(15),
+  surname varchar(25),
+  department varchar(20), 
+  salary int, 
+  details_id int
+,  PRIMARY KEY (id)
+, FOREIGN KEY (details_id) REFERENCES hibernate_db.details(id));
